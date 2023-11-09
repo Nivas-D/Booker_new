ab841068278c2c25be1c5d9467c77caabc628ea0
Nivas-D <nivas.di07@gmail.com>
2023-11-09 14:06:09 +0530

    db model comit...

--- app/Http/Controllers/Admin/EmployeeController.php
+++ app/Http/Controllers/Admin/EmployeeController.php
@@ -2,7 +2,7 @@
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\facades\DB;
+use Illuminate\Support\Facades\DB;
 use App\Models\Employee;
 use Validator;
 
